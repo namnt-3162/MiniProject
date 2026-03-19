fdcfbe221cbeee8ae5530922f69efff544229fde
Nguyễn Thành Nam B <nguyen.thanh.nam-b@sun-asterisk.com>
2026-03-19 14:33:03 +0700

    + Add Module 4:Spring Boot + DB

---
+++ src/main/java/com/example/employee_management/controller/EmployeeController.java
@@ -0,0 +1,56 @@
+package com.example.employee_management.controller;
+
+import com.example.employee_management.model.Department;
+import com.example.employee_management.model.Employee;
+import com.example.employee_management.repository.DepartmentRepository;
+import com.example.employee_management.repository.EmployeeRepository;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private final EmployeeRepository employeeRepository;
+    private final DepartmentRepository departmentRepository;
+
+   public EmployeeController(EmployeeRepository employeeRepository, DepartmentRepository departmentRepository) {
+        this.employeeRepository = employeeRepository;
+        this.departmentRepository = departmentRepository;
+    }
+
+    // Lấy tất cả hoặc tìm kiếm theo tên
+    @GetMapping
+    public List<Employee> getAll(@RequestParam(required = false) String name) {
+        if (name != null) return employeeRepository.findByNameContaining(name);
+        return employeeRepository.findAll();
+    }
+
+   @PostMapping
+    public ResponseEntity<Employee> create(@RequestBody Employee employee) {
+        if (employee.getDepartment() != null) {
+            Department dept = departmentRepository.findById(employee.getDepartment().getId())
+                .orElseThrow(() -> new RuntimeException("Department not found!"));
+        
+            employee.setDepartment(dept);
+        }
+    
+        Employee savedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedEmployee);
+    }
+
+    @GetMapping("/dept/{deptId}")
+    public List<Employee> getByDept(@PathVariable Long deptId) {
+        return employeeRepository.findByDepartmentId(deptId);
+    }
+}
--- src/main/java/com/example/employee_management/model/Employee.java
+++ src/main/java/com/example/employee_management/model/Employee.java
@@ -1,23 +1,39 @@
 package com.example.employee_management.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Employee {
-    private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String position;
-
+    
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
     // Constructors
     public Employee() {}
-    public Employee(String id, String name, String position) {
+    public Employee(Long id, String name, String position, Department department) {
         this.id = id;
         this.name = name;
         this.position = position;
+        this.department = department;
     }
 
     // Getters and Setters (Quan trọng để Spring có thể parse JSON)
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
     public String getPosition() { return position; }
     public void setPosition(String position) { this.position = position; }
+    public Department getDepartment() { return department; }
+    public void setDepartment(Department department) { this.department = department; }
 }
